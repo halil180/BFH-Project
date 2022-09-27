90e43881d69c66f5fc91a359e9ed33c9a3680186
halil180 <halilkom180@.com>
2022-09-27 13:04:37 +0200

    coordinate game added

---
+++ jsFiles/coordinatePractice.js
@@ -0,0 +1,160 @@
+ const  coordinateOneThree = document.getElementById('coordinate-one-three')
+ const  coordinateTwoThree = document.getElementById('coordinate-two-three')
+ const  coordinateThreeThree= document.getElementById('coordinate-three-three')
+ const  coordinateOneTwo= document.getElementById('coordinate-one-two')
+ const  coordinateTwoTwo= document.getElementById('coordinate-two-two')
+ const  coordinateThreeTwo= document.getElementById('coordinate-three-two')
+ const  coordinateOneOne= document.getElementById('coordinate-one-one')
+ const  coordinateTwoOne= document.getElementById('coordinate-two-one')
+ const  coordinateThreeOne= document.getElementById('coordinate-three-one')
+
+ const numOfCorrectAnswers = document.getElementById('numOfCorrectAnswers')
+ const numOfWrongAnswers = document.getElementById('numOfWrongAnswers')
+
+
+
+
+
+
+
+
+// Set the date we're counting down to
+// var setoneminutetime = new Date();
+// setoneminutetime.setTime(Date.now() + 1 * 60 * 1000); // Add 1 minutes to current timestamp
+// var countDownDate = new Date(setoneminutetime).getTime();
+// //set interval for the actual countdown
+// var x = setInterval(function() {
+//     var now = new Date().getTime();
+//     //end time minus the current time
+//     var distance = countDownDate - now;
+
+
+//     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//     //show countdown  in div demo
+//     document.getElementById("demo").innerHTML = "Time left :" + minutes + "m " + seconds + "s ";
+//     if (distance < 0) {
+//         clearInterval(x);
+//         document.getElementById("demo").innerHTML = "EXPIRED";
+//     }
+// }, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ const userInput =document.getElementById('userInput')
+
+let coordinates = [ 
+    {
+        coordinateItem:    coordinateOneThree,
+        coordinateNumber:    13,
+    },
+    {
+        coordinateItem:    coordinateTwoThree,
+        coordinateNumber:    23,
+    },
+    {
+        coordinateItem:    coordinateThreeThree,
+        coordinateNumber:    33,
+    },
+    {
+        coordinateItem:    coordinateOneTwo,
+        coordinateNumber:    12,
+    },
+    {
+        coordinateItem:    coordinateTwoTwo,
+        coordinateNumber:    22,
+    },
+    {
+        coordinateItem:    coordinateThreeTwo,
+        coordinateNumber:    32,
+    },
+    {
+        coordinateItem:    coordinateOneOne,
+        coordinateNumber:    11,
+    },
+    {
+        coordinateItem:    coordinateTwoOne,
+        coordinateNumber:    21,
+    },
+    {
+        coordinateItem:    coordinateThreeOne,
+        coordinateNumber:    31,
+    },
+]
+//  coordinateOneThree.style.background ="red";
+//  coordinateTwoThree.style.background ="red";
+//  coordinateThreeThree.style.background ="red";
+//  coordinateOneTwo.style.background ="red";
+//  coordinateTwoTwo.style.background ="red";
+//  coordinateThreeTwo.style.background ="red";
+//  coordinateOneOne.style.background ="red";
+//  coordinateTwoOne.style.background ="red";
+//  coordinateThreeOne.style.background ="red";
+
+///select a random coordinate from the array of coordinates
+let randomItem = coordinates[Math.floor(Math.random()*coordinates.length)];
+randomItem.coordinateItem.style.background = 'red';
+function getRandomCoordinate() {
+    randomItem = coordinates[Math.floor(Math.random()*coordinates.length)];
+    console.log(randomItem)
+    randomItem.coordinateItem.style.background = 'red';
+}
+
+
+console.log(randomItem)
+// randomItem.style.background ="red";
+let userAnswer = ''
+let correctAnswer = 0;
+let wrongAnswer = 0;
+userInput.addEventListener('input', updateValue);
+userInput.addEventListener('keydown', submitAnswer);
+function submitAnswer(e) {
+    if (13 == e.keyCode) {
+        if (userAnswer == randomItem.coordinateNumber) {
+            userInput.value =''
+            console.log('correct answer')
+            randomItem.coordinateItem.style.background = '#37556E';
+            ++correctAnswer
+            numOfCorrectAnswers.innerHTML =`correct : ${correctAnswer}` 
+            getRandomCoordinate()
+
+        }else{
+            userInput.value =''
+            randomItem.coordinateItem.style.background = '#37556E';
+            getRandomCoordinate();
+
+            ++wrongAnswer
+
+            numOfWrongAnswers.innerHTML = `wrong : ${wrongAnswer}` 
+
+        }
+       console.log('answer ', userAnswer)
+    }
+  }
+function updateValue(e) {
+    userAnswer = e.target.value;
+ console.log(e.target.value);
+}
+
+
+
+
+
+
+
+
+
+
+
